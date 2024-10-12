4fd06d321d292bfd0c0720a7b072549a91ed83db
mrgizmo212 <info@truetradinggroup.com>
2024-10-12 17:24:22 -0400

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -45,6 +45,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${port}`);
 });
