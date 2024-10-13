c01796537265748d94677bcba7bae007174c832a
mrgizmo212 <info@truetradinggroup.com>
2024-10-13 09:53:27 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
+import React from 'react';
 import { ConsolePage } from './pages/ConsolePage';
+import { ThemeProvider, useTheme } from './ThemeContext';
 import './App.scss';
+
+const ThemeToggle: React.FC = () => {
+  const { theme, toggleTheme } = useTheme();
+  return (
+    <button onClick={toggleTheme} className="theme-toggle">
+      {theme === 'light' ? '🌙' : '☀️'}
+    </button>
+  );
+};
+
+const AppContent: React.FC = () => {
+  return (
+    <div data-component="App">
+      <ThemeToggle />
+      <ConsolePage />
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div data-component="App">
-      <ConsolePage />
-    </div>
+    <ThemeProvider>
+      <AppContent />
+    </ThemeProvider>
   );
 }
 
