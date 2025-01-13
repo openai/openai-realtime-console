a62a4596a919b6a21a46608691cb37b191dcfeec
Peiyong <abupeiyong@gmail.com>
2025-01-13 15:19:12 +0400

    stop audio capture when session closed

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -64,6 +64,12 @@ export default function App() {
     if (dataChannel) {
       dataChannel.close();
     }
+    peerConnection.current.getSenders().forEach(sender => {
+      if (sender.track) {
+        sender.track.stop();
+      }
+    });
+    
     if (peerConnection.current) {
       peerConnection.current.close();
     }
