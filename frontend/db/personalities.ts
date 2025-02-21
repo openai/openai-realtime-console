741533bc5376f2b0e1a522c54ced110ce1f16997
akdeb <deb2@illinois.edu>
2025-02-21 14:19:42 +0000

    update orders

--- frontend/db/personalities.ts
+++ frontend/db/personalities.ts
@@ -1,7 +1,10 @@
 import { SupabaseClient } from "@supabase/supabase-js";
 
 export const getAllPersonalities = async (supabase: SupabaseClient) => {
-    const { data, error } = await supabase.from("personalities").select("*");
+    const { data, error } = await supabase
+        .from("personalities")
+        .select(`*`)
+        .order("created_at", { ascending: false });
     if (error) {
         console.log("error getAllPersonalities", error);
         return [];
